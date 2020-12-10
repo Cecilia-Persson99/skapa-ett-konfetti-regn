ae51f2f35b9a77047cf7dd89b30e29db28653389
Cecilia Persson <75508085+Cecilia-Persson99@users.noreply.github.com>
2020-12-10 22:17:37 +0100

    made it rain :)

--- confetti.js
+++ confetti.js
@@ -1,3 +1,12 @@
+let farger = [
+  "Yellow",
+  "Orange",
+  "Salmon",
+  "Aquamarine",
+  "Steelblue",
+  "Seagreen",
+];
+
 // Om du tittar på projektet i browsern ser du en ensam grå
 // och lite sorglig konfettipartikel. Din uppgift blir att
 // fylla konfettianimationen med färger och liv!
@@ -9,9 +18,9 @@
 // t.ex. baserat på slump eller baserat på inparametern groupName
 // så kan det istället bli en blandning av färger.
 const chooseParticleColor = (groupName) => {
-  return 'gray'
-}
-
+  const randomColor = farger[Math.floor(Math.random() * farger.length)];
+  return randomColor;
+};
 
 // Denna funktion anropas för att skjuta ut konfetti. Funktionen
 // som kommer in som parameter addConfettiParticles kan anropas en
@@ -25,21 +34,138 @@ const bang = (addConfettiParticles) => {
   // (och med vilken hastighet) de flyger, hur mycket partiklarna
   // sprider ut sig från varandra samt ett gruppnamn som skickas in i
   // chooseParticleColor. Ändra på dessa värden och se vad som händer!
-  addConfettiParticles({
-    groupName: 'lonely sad confetti',  // Valfritt namn på gruppen
-    particleAmount: 1,  // Antal konfettipartiklar
-    xPosition: 50,      // Position i procent av skärmens bredd, 0 är längst till vänster och 100 längst till höger
-    yPosition: 50,      // Position i procent av skärmens höjd, 0 är längst ner och 100 högst upp
-    xVelocity: 0,       // Hastighet i sidled, positiva värden åker åt höger och negativa åt vänster
-    yVelocity: 2,       // Hastighet i höjdled, positiva värden åker uppåt och negativa värden nedåt
-    xSpread: 80,        // Slumpmässig spridning på partikelns hastighet i sidled
-    ySpread: 20         // Slumpmässig spridning på partikelns hastighet i höjdled
-  })
-}
+
+  setTimeout(function () {
+    addConfettiParticles({
+      groupName: "lonely sad confetti", // Valfritt namn på gruppen
+      particleAmount: 1200, // Antal konfettipartiklar
+      xPosition: 10, // Position i procent av skärmens bredd, 0 är längst till vänster och 100 längst till höger
+      yPosition: 100, // Position i procent av skärmens höjd, 0 är längst ner och 100 högst upp
+      xVelocity: 0, // Hastighet i sidled, positiva värden åker åt höger och negativa åt vänster
+      yVelocity: 2, // Hastighet i höjdled, positiva värden åker uppåt och negativa värden nedåt
+      xSpread: 80, // Slumpmässig spridning på partikelns hastighet i sidled
+      ySpread: 20, // Slumpmässig spridning på partikelns hastighet i höjdled
+    });
+  }, 0);
+
+  setTimeout(function () {
+    addConfettiParticles({
+      groupName: "lonely sad confetti",
+      particleAmount: 1200,
+      xPosition: 20,
+      yPosition: 120,
+      xVelocity: 0,
+      yVelocity: 2,
+      xSpread: 220,
+      ySpread: 80,
+    });
+  }, 0);
+
+  setTimeout(function () {
+    addConfettiParticles({
+      groupName: "lonely sad confetti",
+      particleAmount: 1200,
+      xPosition: 30,
+      yPosition: 120,
+      xVelocity: 0,
+      yVelocity: 2,
+      xSpread: 220,
+      ySpread: 80,
+    });
+  }, 0);
+
+  setTimeout(function () {
+    addConfettiParticles({
+      groupName: "lonely sad confetti",
+      particleAmount: 1200,
+      xPosition: 40,
+      yPosition: 120,
+      xVelocity: 0,
+      yVelocity: 2,
+      xSpread: 220,
+      ySpread: 80,
+    });
+  }, 0);
+
+  setTimeout(function () {
+    addConfettiParticles({
+      groupName: "lonely sad confetti",
+      particleAmount: 1200,
+      xPosition: 50,
+      yPosition: 120,
+      xVelocity: 0,
+      yVelocity: 2,
+      xSpread: 220,
+      ySpread: 80,
+    });
+  }, 0);
+
+  setTimeout(function () {
+    addConfettiParticles({
+      groupName: "lonely sad confetti",
+      particleAmount: 1200,
+      xPosition: 60,
+      yPosition: 120,
+      xVelocity: 0,
+      yVelocity: 2,
+      xSpread: 220,
+      ySpread: 80,
+    });
+  }, 0);
+
+  setTimeout(function () {
+    addConfettiParticles({
+      groupName: "lonely sad confetti",
+      particleAmount: 1200,
+      xPosition: 70,
+      yPosition: 120,
+      xVelocity: 0,
+      yVelocity: 2,
+      xSpread: 220,
+      ySpread: 80,
+    });
+  }, 0);
+
+  setTimeout(function () {
+    addConfettiParticles({
+      groupName: "lonely sad confetti",
+      particleAmount: 1200,
+      xPosition: 80,
+      yPosition: 120,
+      xVelocity: 0,
+      yVelocity: 2,
+      xSpread: 220,
+      ySpread: 80,
+    });
+  }, 0);
+
+  setTimeout(function () {
+    addConfettiParticles({
+      groupName: "lonely sad confetti",
+      particleAmount: 1200,
+      xPosition: 90,
+      yPosition: 120,
+      xVelocity: 0,
+      yVelocity: 2,
+      xSpread: 220,
+      ySpread: 80,
+    });
+  }, 0);
+
+  setTimeout(function () {
+    addConfettiParticles({
+      groupName: "lonely sad confetti",
+      particleAmount: 1200,
+      xPosition: 100,
+      yPosition: 120,
+      xVelocity: 0,
+      yVelocity: 2,
+      xSpread: 220,
+      ySpread: 80,
+    });
+  }, 0);
+};
 
 // Här exporterar vi funktionerna så att de kan användas i
 // confetti-creator-filen som du inte behöver bry dig om
-export {
-  chooseParticleColor,
-  bang
-}
+export { chooseParticleColor, bang };
